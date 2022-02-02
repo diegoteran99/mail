a8e89db22cf21228ef40ced99371c539adddbb09
diegoteran99 <diegoteranbaselli@gmail.com>
2022-02-02 13:44:52 -0300

    second commit

--- resources/views/emails/contact.blade.php
+++ resources/views/emails/contact.blade.php
@@ -1,7 +1,28 @@
 @component('mail::message')
 # Introduction
 
-Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur odit voluptate deserunt error totam est alias quos deleniti quo. Dolores, voluptatum vitae pariatur maiores ut delectus eos repudiandae impedit esse.
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur odit voluptate deserunt error totam est alias quos deleniti quo. Dolores, voluptatum vitae pariatur maiores 
+ut delectus eos repudiandae impedit esse.
+
+<div style="display: inline-flex;">
+<div style="margin-right: 2rem; text-align: justify;">
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus accusantium maiores maxime nihil. Placeat esse excepturi inventore nam ab repellendus dignissimos velit minima! 
+Inventore labore itaque ipsum reiciendis molestias sed. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem eligendi et iste ad corporis illum in repellendus, 
+itaque tenetur quos quae quibusdam nobis earum eum similique maiores autem maxime temporibus.
+</div>
+
+<div style="text-align: justify;">
+Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui accusamus perspiciatis, odio temporibus quia in corporis delectus dolores provident distinctio, voluptatem 
+assumenda beatae voluptates. Reiciendis nihil cumque incidunt similique? Amet. Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus excepturi corrupti 
+libero ut id nisi fugiat voluptas. Architecto, doloribus. Libero dolorem illum sequi, nesciunt minima impedit blanditiis labore nihil quam?
+</div>
+</div>
+
+- Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+
+- Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+
+- Lorem ipsum dolor sit amet consectetur adipisicing elit.
 
 <img src="http://justbetter.cl/img/logo.png" alt="" style="max-width: 40%; background-color: black;">    
 
